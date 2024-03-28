1fa1b24449f889a8a73ca792d299f0dc0681b7fe
Maryna <127458923+marnka@users.noreply.github.com>
2024-03-28 23:01:24 +0200

    Add files via upload

---
+++ task 1/test_1.py
@@ -0,0 +1,4 @@
+def test_1():
+    result = addition(5, 3)
+    print(f"Результат додавання: {result}")
+    assert result == 8
---
+++ task 1/test_3.py
@@ -0,0 +1,2 @@
+def test_sum_negative_numbers():
+    assert addition(-5, -3) == -8
