ace4031a9605f0756dcbec938968f3f292944d49
Maryna <127458923+marnka@users.noreply.github.com>
2024-06-01 12:03:46 +0300

    Update all_tests.py

--- all_tests.py
+++ all_tests.py
@@ -1,28 +1,7 @@
 import pytest
 from dumb_code import addition
 
-def test_1():
-  result = addition(5, 3)
-  print(f"Результат додавання: {result}")
-  assert result == 8
-
-
-if __name__ == "__main__":
-  test_1()
-
-def test_2():
-  result = addition(5, -3)
-  print(f"Результат додавання: {result}")
-  assert result == 2
-
-if __name__ == "__main__":
-  test_2()
-
-
-def test_3():
-  result = addition(-5, -3)
-  print(f"Результат додавання: {result}")
-  assert result == -8
-
-if __name__ == "__main__":
-  test_3()
+def test_addition():
+    assert addition(5, 3) == 8
+    assert addition(5, -3) == 2
+    assert addition(-5, -3) == -8
