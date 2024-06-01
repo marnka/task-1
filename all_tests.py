9efe6a8f473b270fd392eb0c78b6c3b277116bac
Maryna <127458923+marnka@users.noreply.github.com>
2024-06-01 11:52:23 +0300

    Update all_tests.py

--- all_tests.py
+++ all_tests.py
@@ -1,28 +1,14 @@
 import pytest
 from dumb_code import addition
 
-def test_1():
-    result = addition(5, 3)
+@pytest.mark.parametrize(
+    "a, b, expected_result", [
+        (5, 3, 8),
+        (5, -3, 2),
+        (-5, -3, -8),
+    ]
+)
+def test_addition(a, b, expected_result):
+    result = addition(a, b)
     print(f"Результат додавання: {result}")
-    assert result == 8
-
-
-if __name__ == "__main__":
-    test_1()
-
-def test_2():
-    result = addition(5, -3)
-    print(f"Результат додавання: {result}")
-    assert result == 2
-
-if __name__ == "__main__":
-    test_2()
-
-
-def test_3():
-    result = addition(-5, -3)
-    print(f"Результат додавання: {result}")
-    assert result == -8
-
-if __name__ == "__main__":
-    test_3()
+    assert result == expected_result
